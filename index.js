06ea3b08ae61b5f757362521f45bc56181506dea
abcmehedi5 <abcmehedi5@gmail.com>
2023-06-07 18:53:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+require("dotenv").config();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.13ytubh.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    const db = client.db("design-school");
+    const userCollection = db.collection("users");
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+// design school api start ------------------------------
+
+
+
+// design school api end ------------------------------
+
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("design-school server running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Design-School server is running ${PORT}`);
+});
